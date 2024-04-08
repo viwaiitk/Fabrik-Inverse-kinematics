f45f30ab2423a9156555c47fa353df8c4c07c112
viwaiitk <166342258+viwaiitk@users.noreply.github.com>
2024-04-08 17:04:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,18 +2,10 @@ from fabrikSolver import FabrikSolver2D, FabrikSolver3D
 
 arm = FabrikSolver3D()
 
-arm.addSegment(100, 0, 0)
-arm.addSegment(100, 0, 0)
+arm.addSegment(23, 0, 0)
+arm.addSegment(15, 0, 0)
 
-arm.compute(100, 150, 50)
-
-arm.plot()
-
-arm = FabrikSolver2D()
-
-arm.addSegment(100, 30)
-arm.addSegment(100, 70)
-
-arm.compute(100, 70)
+arm.compute(7, 0, 0)
 
 arm.plot()
+
